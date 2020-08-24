ac171ac49149c6903460bb7b2377325bcefcd7ee
Christina Yujeong Yun <yjc.yun@gmail.com>
2020-08-24 19:16:41 -0400

    Add scrollToTop component from react-router-dom

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import ScrollToTop from './components/ScrollToTop';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </Provider>,
